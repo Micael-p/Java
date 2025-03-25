ed3419cf882997f132a2841c12b7a4431c60752c
Micael-p <micaelpessoa5@gmail.com>
2025-03-25 09:40:55 -0300

    Main.java

--- Orientação_De_Obg/Aula_03/Main.java
+++ Orientação_De_Obg/Aula_03/Main.java
@@ -1,10 +1,14 @@
 import java.util.Date;
 public class Main {
+    
     public static void main(String[] args) {
+        
+        // criando uma instancia de Aluno
         Aluno aluno = new Aluno("Micael", "123.456.789-00", new Date(), 1001);
         aluno.setNome("Micael Pessoa Da Silva - ");
         System.out.println("Aluno: " + aluno.getNome() + "Matricula: " + aluno.getMatricula());
 
+        // Criando uma instancia de Empregado
         Empregado empregado = new Empregado ("Maria", "987.654.321-00", new Date(), 3000.00);
         System.out.println("Empregado: " + empregado.getNome() + " - Salario: " + empregado.getSalario());
     }
