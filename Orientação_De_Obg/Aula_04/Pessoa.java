b6bdad2d706ac61e651165b1d4acfa13f25ba2dc
Micael-p <micaelpessoa5@gmail.com>
2025-04-01 10:08:58 -0300

    Pessoa.java

---
+++ Orientação_De_Obg/Aula_04/Pessoa.java
@@ -0,0 +1,11 @@
+abstract class Pessoa {
+    private String nome;
+
+    public String getNome () {
+        return nome;
+    }
+
+    public void setNome(final String nome) {
+        this.nome = nome;
+    }
+}
